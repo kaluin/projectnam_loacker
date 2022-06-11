7450b790ad8e9db4a0fc03b7120f7ad840aeb45a
kaluin <62045964+kaluin@users.noreply.github.com>
2022-06-11 12:16:25 +0900

    Update manageDB.py

--- projectnam_locker/manageDB.py
+++ projectnam_locker/manageDB.py
@@ -2,7 +2,7 @@ from sys import exit
 from mariadb import connect, Error
 
 try:
-    conn = connect(user="root",password="wjddn854",host="127.0.0.1",port=3306,database="projectnam")
+    conn = connect(user="root",password="****",host="127.0.0.1",port=3306,database="projectnam")
 except Error as e:
     print("Error Connecting to MariaDB Platform")
     exit(1)
